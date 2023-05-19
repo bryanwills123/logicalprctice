e0c015e1cd28053a5ad00b7ecbf28fe40fec7c0e
BapparayaR <BapparayaR@GSK-QA-065.grasko.com>
2023-05-19 14:06:40 +0530

    first commit

---
+++ logicalpractice/src/logicalpractice/Primenumber.java
@@ -0,0 +1,27 @@
+package logicalpractice;
+
+public class Primenumber {
+
+	public static void main(String[] args) {
+		
+		int number = 8;
+		int count=0;
+		
+		if(number>0) {
+			for(int i=1;i<=number;i++) {
+				if( number%i==0) {
+				count++;
+				}
+				
+		}
+			if(count==2) {
+				System.out.println("it is a prime number");
+			}
+			else {
+				System.out.println("it is not a prime number");
+			}
+		}else {System.out.println("given number is not a prime number");}
+
+	}
+
+}
